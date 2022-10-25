77759060ed685f000358e6ec262915d8b98cde8f
Lasse Suomela <suomelasse@gmail.com>
2022-10-25 17:31:44 +0300

    added dockerfiles, docker compose files, removed todo.txt

--- frontend/src/components/ChampionDetails.js
+++ frontend/src/components/ChampionDetails.js
@@ -4,7 +4,7 @@ export default function ChampionDetails(props) {
 
   return (
     <div className="container text-center">
-        <div className="row mb-2 ">
+        <div className="row mb-2">
             <div className="col-1"></div>
             <div className="col-1">
                 <img src={"https://ddragon.leagueoflegends.com/cdn/12.19.1/img/champion/" + props.championKey + ".png"} alt={props.championKey} className="championData"/>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,10 +12,10 @@ export default function Header() {
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <Link class="nav-link" to="register">Register</Link>
+            <Link class="nav-link" to="scoreboard">Scoreboard</Link>
             </li>
             <li class="nav-item">
-            <Link class="nav-link" to="scoreboard">Scoreboard</Link>
+              <Link class="nav-link" to="register">Register</Link>
             </li>
           </ul>
         </div>
