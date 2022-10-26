24af6802bf28767ed0423b8da38bd4a0c42ad4d0
Lasse Suomela <suomelasse@gmail.com>
2022-10-26 19:55:12 +0300

    tried some stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Game from "./components/Game";
-import ScoreBoard from "./views/ScoreBoard";
+import Scoreboard from "./views/Scoreboard";
 import Register from "./views/Register";
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Game />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/scoreboard" element={<ScoreBoard />} />
+            <Route path="/scoreboard" element={<Scoreboard />} />
             <Route path="*" element={<Navigate to="/"/>} />
           </Routes>
         </div>
