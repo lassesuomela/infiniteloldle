4d4132b13edd366ba82bc5df605cec5fb69f15c6
Lasse Suomela <suomelasse@gmail.com>
2022-10-27 15:58:54 +0300

    changed class to className, added favicon, added logo to navbbar

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -3,19 +3,20 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark mb-5">
-      <div class="container-fluid">
-        <Link class="navbar-brand" to="/">Infiniteloldle</Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark mb-5">
+      <div className="container-fluid">
+        <img src={"./favicon.png"} className="m-2" />
+        <Link className="navbar-brand" to="/">Infiniteloldle</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-            <Link class="nav-link" to="scoreboard">Scoreboard</Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+            <Link className="nav-link" to="scoreboard">Scoreboard</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="register">Register</Link>
+            <li className="nav-item">
+              <Link className="nav-link" to="register">Register</Link>
             </li>
           </ul>
         </div>
