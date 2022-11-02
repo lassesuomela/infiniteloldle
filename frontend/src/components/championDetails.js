f718b1c133035a7a6910f054897f3fe0e659cbad
Lasse Suomela <suomelasse@gmail.com>
2022-11-02 20:45:48 +0200

    css changes, border is now dark gray, added linear gradient to year, compacted code

--- frontend/src/components/championDetails.js
+++ frontend/src/components/championDetails.js
@@ -9,25 +9,25 @@ export default function ChampionDetails(props) {
             <div className="col-1">
                 <img src={"https://ddragon.leagueoflegends.com/cdn/12.19.1/img/champion/" + props.championKey + ".png"} alt={props.championKey} className="championData"/>
             </div>
-            <div className={props.similarites.sameGender ? "col-1 championData correct" : "col-1 championData incorrect"} id="gender">
+            <div className={"col-1 championData " + (props.similarites.sameGender ? "correct" : "incorrect")} id="gender">
                 <span className="align-center">{props.gender === 1 ? "Male" : props.gender === 2 ? "Female" : "Other"}</span>
             </div>
-            <div className={props.similarites.sameResource ? "col-1 championData correct" : "col-1 championData incorrect"} id="resource">
+            <div className={"col-1 championData " + (props.similarites.sameResource ? "correct" : "incorrect")} id="resource">
                 <span className="align-center">{props.resource}</span>
             </div>
-            <div className={props.similarites.sameRangeType === "partial" ? "col-1 championData partial" : props.similarites.sameRangeType ? "col-1 championData correct" : "col-1 championData incorrect"} id="range">
+            <div className={"col-1 championData " + (props.similarites.sameRangeType === "partial" ? "partial" : props.similarites.sameRangeType ? "correct" : "incorrect")} id="range">
                 <span className="align-center">{props.rangeTypes.replace(/,/g, " ")}</span>
             </div>
-            <div className={props.similarites.sameGenre === "partial" ? "col-2 championData partial" : props.similarites.sameGenre ? "col-2 championData correct" : "col-2 championData incorrect"} id="genre">
+            <div className={"col-2 championData " + (props.similarites.sameGenre === "partial" ? "partial" : props.similarites.sameGenre ? "correct" : "incorrect")} id="genre">
                 <span className="align-center breakWord">{props.genre.replace(/,/g, " ")}</span>
             </div>
-            <div className={props.similarites.samePosition === "partial" ? "col-2 championData partial" : props.similarites.samePosition ? "col-2 championData correct" : "col-2 championData incorrect"} id="position">
+            <div className={"col-2 championData " + (props.similarites.samePosition === "partial" ? "partial" : props.similarites.samePosition ? "correct" : "incorrect")} id="position">
                 <span className="align-center breakWord">{props.positions.replace(/,/g, " ")}</span>
             </div>
-            <div className={props.similarites.sameReleaseYear === "=" ? "col-1 championData correct" : "col-1 championData incorrect"} id="released">
-                <span className="align-center">{props.similarites.sameReleaseYear + props.releaseYear}</span>
+            <div className={"col-1 championData " + (props.similarites.sameReleaseYear === "=" ? "correct" : props.similarites.sameReleaseYear === "<" ? "incorrect-less" : props.similarites.sameReleaseYear === ">" ? "incorrect-greater" : "")} id="released">
+                <span className="align-center">{props.releaseYear}</span>
             </div>
-            <div className={props.similarites.sameRegion ? "col-2 championData correct" : "col-2 championData incorrect"} id="region">
+            <div className={"col-2 championData " + (props.similarites.sameRegion ? "correct" : "incorrect")} id="region">
                 <span className="align-center">{props.regions}</span>
             </div>
         </div>
