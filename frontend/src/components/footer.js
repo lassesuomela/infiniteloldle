4bf326c27808f0e94809347a05b05863a22fc636
Lasse Suomela <suomelasse@gmail.com>
2022-11-03 18:25:12 +0200

    created sitemap, added sitemap to robots txt

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div id="footer" className="text-center fixed-bottom">
-      <p className="mt-3">{new Date().getFullYear()} infiniteloldle.com</p>
+    <div id="footer" className="fixed-bottom">
+      <div className="text-center">
+        <p className="mt-3">{new Date().getFullYear()} infiniteloldle.com</p>
+      </div>
     </div>
   )
 }
