e2051e70102ac05edd1a83efd405e050d304556e
Lasse Suomela <suomelasse@gmail.com>
2022-11-12 15:44:13 +0200

    added another disclaimer

--- frontend/src/components/legal.js
+++ frontend/src/components/legal.js
@@ -5,6 +5,7 @@ export default function Legal() {
     <div>
       <h3>Disclaimer</h3>
       <p>Infiniteloldle.com isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</p>
+      <p>Infiniteloldle.com was created under Riot Games' "Legal Jibber Jabber" policy using assets owned by Riot Games. Riot Games does not endorse or sponsor this project.</p>
     </div>
   )
 }
