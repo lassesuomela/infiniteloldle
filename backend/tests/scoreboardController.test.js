1a4fb735f9a22cb23c5a62062e4f492af40f2984
Lasse Suomela <suomelasse@gmail.com>
2022-11-13 21:39:41 +0200

    added scoreboard test

---
+++ backend/tests/scoreboardController.test.js
@@ -0,0 +1,19 @@
+const app = require("../app");
+const request = require("supertest");
+
+describe("Testing scoreboard routes", () => {
+
+    it("Fetching top 10 players.", (done) => {
+
+        request(app)
+            .get("/api/scoreboard")
+
+            .then(res => {
+                expect(res.body.status).toBe("success");
+                expect(res.body).toHaveProperty("scores");
+
+                done();
+            })
+    });
+
+});
