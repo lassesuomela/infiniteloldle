5f9208ad6e83e04a11e4b3d08c2240fada138c96
Lasse Suomela <suomelasse@gmail.com>
2022-11-18 19:06:56 +0200

    db fix

--- backend/mysql-docker/db.sql
+++ backend/mysql-docker/db.sql
@@ -22,7 +22,6 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `loldle` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `loldle`;
--
 
 --
 -- Table structure for table `champions`
