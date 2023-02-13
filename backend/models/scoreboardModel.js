718d3247bad5c5f976c533c5659590429640f76b
Lasse Suomela <suomelasse@gmail.com>
2023-02-13 22:30:25 +0200

    fixed player count naming in sql query

--- backend/models/scoreboardModel.js
+++ backend/models/scoreboardModel.js
@@ -2,7 +2,7 @@ const db = require('../configs/db');
 
 const scorebaord = {
     getByScoreCount: (cb) =>{
-        return db.query("SELECT nickname, timestamp, score, prestige FROM users ORDER BY score DESC limit 10; SELECT COUNT(id) FROM users", cb)
+        return db.query("SELECT nickname, timestamp, score, prestige FROM users ORDER BY score DESC limit 10; SELECT COUNT(id) AS 'playerCount' FROM users", cb)
     }
 }
 
