b1a378b468faad57c40350d38c094e95bcd128ba
Lasse Suomela <suomelasse@gmail.com>
2023-04-14 16:35:21 +0300

    Added more statistics, added rechart to show charts on /stats page, added user stats from 50 top players

--- backend/controllers/statsController.js
+++ backend/controllers/statsController.js
@@ -2,16 +2,32 @@ const statsModel = require("../models/statsModel");
 
 const GetAll = (req, res) => {
   statsModel.getAll((err, result) => {
-    if (!result || result.length === 0) {
+    if(err) {
+      console.log(err);
+      return res.status(500).json({status:"error", msg:"No statistics data found"})
+    }
+    if (result[0].length === 0) {
       return res.json({
         status: "error",
         error: "No results found",
       });
     }
 
+    let globalSkinCount = 0;
+
+    result[5].forEach(champion => {
+      globalSkinCount += champion.skinCount;
+    })
+
     const response = {
       status: "success",
-      stats: result,
+      stats: result[0],
+      register_count: result[1][0].user_count,
+      player_count: result[2][0].player_count,
+      item_count: result[3][0].item_count,
+      champion_count: result[4][0].champion_count,
+      global_skin_count: globalSkinCount,
+      player_stats: result[6]
     };
 
     res.json(response);
--- frontend/src/components/statsData.js
+++ frontend/src/components/statsData.js
@@ -1,34 +1,122 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import Config from "../configs/config";
+import { BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 export default function StatsData() {
   const [stats, setStats] = useState([]);
+  const [otherStat, setOtherStat] = useState([]);
+  const [msg, setMsg] = useState("");
+
   useEffect(() => {
     axios
       .get(Config.url + "/stats")
       .then((response) => {
         setStats(response.data.stats);
-        console.log(response.data);
+
+        const data = {
+          register_count: response.data.register_count,
+          player_count: response.data.player_count,
+          item_count: response.data.item_count,
+          champion_count: response.data.champion_count,
+          global_skin_count: response.data.global_skin_count,
+          player_stats: response.data.player_stats
+        }
+        setOtherStat(data);
       })
       .catch((error) => {
         console.log(error);
+        if(error.response.data.msg) {
+          setMsg(error.response.data.msg);
+        }
       });
   }, []);
+
+  const data = stats.map(stat => {
+    return ({
+      date: new Date(stat.date).toLocaleDateString(undefined, {month: "numeric", day: "numeric"}),
+      DAU: stat.dau,
+      Requests: stat.requests,
+    });
+  })
+
+  if(msg.length > 0) {
+    console.log("asdsada");
+    return (
+    <>
+      <h3 className="text-center">{msg}</h3>
+    </>
+    )
+  }
 
   return (
     <>
       <h3 className="text-center pb-3 pt-4">Statistics</h3>
-      <div className="d-flex justify-content-center text-center">
-        {stats.map((stat) => (
+      <div className="d-grid justify-content-center text-center">
+        <div className="d-flex justify-content-between pt-4 pb-4">
+
           <div>
-            <p>{stat.id}</p>
-            <p>{new Date(stat.date).toLocaleDateString()}</p>
-            <p>{stat.dau}</p>
-            <p>{stat.requests}</p>
+            <h5>Registered users</h5>
+            <p>{otherStat.register_count}</p>
           </div>
-        ))}
+          <div>
+            <h5>Actual players</h5>
+            <p>{otherStat.player_count}</p>
+          </div>
+          <div>
+            <h5>Item count</h5>
+            <p>{otherStat.item_count}</p>
+          </div>
+          <div>
+            <h5>Champion count</h5>
+            <p>{otherStat.champion_count}</p>
+          </div>
+
+          <div>
+            <h5>Skin count</h5>
+            <p>{otherStat.global_skin_count}</p>
+          </div>
+        </div>
+        <h4>Requests per day</h4>
+        <div className="pb-4 pt-2">
+          <LineChart
+            width={1000}
+            height={500}
+            data={data}
+            >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="Requests" stroke="#005A82" />
+          </LineChart>
+        </div>
+        <h4>Daily active users</h4>
+        <div className="pb-4 pt-2">
+          <BarChart width={1000} height={500} data={data}>
+            <Bar dataKey="DAU" fill="#005A82" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+          </BarChart>
+        </div>
+        <h4>Top 50 players</h4>
+        <div className="pb-4 pt-2">
+          <LineChart
+            width={1000}
+            height={500}
+            data={otherStat.player_stats}
+            >
+            <CartesianGrid strokeDasharray="2 2" />
+            <XAxis dataKey="nickname" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="score" stroke="#005A82" />
+          </LineChart>
+        </div>
       </div>
     </>
   );
