ddf95b8d69d6c174e179040a60d52c67090f17e3
Lasse Suomela <101367603+lassesuomela@users.noreply.github.com>
2023-05-30 21:59:25 +0300

    Update db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,7 +3,7 @@ const mysql2 = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql2.createPool({
-  host: "loldle-mysql",
+  host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
