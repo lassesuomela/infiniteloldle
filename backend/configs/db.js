c29b7b938b76544ac065fcf9493d001667945ac5
Lasse Suomela <suomelasse@gmail.com>
2023-05-31 20:16:07 +0300

    Some debug stuff

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,11 +3,21 @@ const mysql2 = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql2.createPool({
-  host: process.env.DB_HOST,
+  host: "loldle-mysql",
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   multipleStatements: true,
 });
+
+pool.query("SELECT * FROM items LIMIT 1", (error, result) => {
+  if (error) throw error;
+  console.log("Db is connected");
+});
+
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_DATABASE);
 
 module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,15 @@ const port = process.env.DOCKER_APP_PORT || 8081;
 
 // start the server and bind to all interfaces
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 
 // handle ctrl + c
-process.on("SIGINT", function() {
-    console.log("Caught interrupt signal");
-    db.end(function (err) {
-        // all connections in the pool have ended
-        console.log("Closed all pool connections");
-        process.exit();
-    });
+process.on("SIGINT", function () {
+  console.log("Caught interrupt signal");
+  db.end(function (err) {
+    // all connections in the pool have ended
+    console.log("Closed all pool connections");
+    process.exit();
+  });
 });
