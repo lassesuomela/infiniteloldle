68dae02dd2e3625d199444019e40b4c465b29d06
Lasse Suomela <suomelasse@gmail.com>
2023-06-21 22:36:22 +0300

    Fixed waf, added err handling, and removed os name as google bot doesnt have that

--- backend/middleware/waf.js
+++ backend/middleware/waf.js
@@ -2,13 +2,14 @@ const Parser = require("ua-parser-js");
 
 const checkRequest = (req, res, next) => {
   const ua = req.get("User-Agent");
+  if (ua === undefined || ua === "-") {
+    return res.status(444);
+  }
   if (ua.includes("wget") || ua.includes("curl") || ua.includes("python")) {
-    console.log("Bad request UA. Closing connection." + ua);
     return res.status(444);
   }
   const parsedUA = new Parser(ua).getResult();
-  if (parsedUA.browser.name === undefined || parsedUA.os.name === undefined) {
-    console.log("Bad request UA. Closing connection." + ua);
+  if (parsedUA.browser.name === undefined) {
     return res.status(444);
   }
   next();
