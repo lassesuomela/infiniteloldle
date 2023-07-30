d5ac641264f647f1ae349ac43172f33cea81ff61
Lasse Suomela <suomelasse@gmail.com>
2023-07-30 21:25:57 +0300

    Reworded some titles in my stats page, added some more stats

--- frontend/src/components/myStats.js
+++ frontend/src/components/myStats.js
@@ -13,7 +13,12 @@ export default function StatsData() {
   const [triesPerDay, setTriesPerDay] = useState([]);
   const [firstTriesPerDay, setFirstTriesPerDay] = useState([]);
   const [scoresPerDay, setScoresPerDay] = useState([]);
+
   const [noData, setNoData] = useState(false);
+
+  const [totalGames, setTotalGames] = useState(0);
+  const [totalGuesses, setTotalGuesses] = useState(0);
+  const [totalFirstTries, setTotalFirstTries] = useState(0);
 
   useEffect(() => {
     if (!localStorage.getItem("token")) {
@@ -41,20 +46,30 @@ export default function StatsData() {
       initValues();
     }
 
-    setGamesPerDay(parse(gamesPlayedData));
-    setTriesPerDay(parse(triesPerDayData));
-    setFirstTriesPerDay(parse(firstTriesPerDayData));
-    setScoresPerDay(parse(scoresPerDayData));
+    const gamesData = parse(gamesPlayedData);
+    const triesData = parse(triesPerDayData);
+    const firstTriesData = parse(firstTriesPerDayData);
+    const scoredData = parse(scoresPerDayData);
+
+    setGamesPerDay(gamesData[0]);
+    setTriesPerDay(triesData[0]);
+    setFirstTriesPerDay(firstTriesData[0]);
+    setScoresPerDay(scoredData[0]);
+
+    setTotalGames(gamesData[1]);
+    setTotalGuesses(triesData[1]);
+    setTotalFirstTries(firstTriesData[1]);
   }, []);
 
   const parse = (data) => {
     if (data === null) {
-      return [];
+      return [[], 0];
     }
     const parsedData = JSON.parse(data);
     const days = Object.keys(parsedData);
 
     const dataPoints = [];
+    let total = 0;
 
     for (const day of days) {
       dataPoints.push({
@@ -63,8 +78,9 @@ export default function StatsData() {
         }),
         Value: parsedData[day].value,
       });
+      total += parsedData[day].value;
     }
-    return dataPoints;
+    return [dataPoints, total];
   };
 
   if (noData) {
@@ -79,7 +95,36 @@ export default function StatsData() {
     <>
       <h3 className="text-center pb-3 pt-4">My stats</h3>
       <div className="d-grid justify-content-center text-center card p-5">
-        <h4>Games per day</h4>
+        <div className="d-flex justify-content-between pb-4">
+          <div>
+            <h5>Total games played</h5>
+            <p>{totalGames}</p>
+          </div>
+          <div>
+            <h5>Total guesses</h5>
+            <p>{totalGuesses}</p>
+          </div>
+          <div>
+            <h5>Average guesses per game</h5>
+            <p>
+              {totalGames !== 0 ? (totalGuesses / totalGames).toFixed(2) : 0}
+            </p>
+          </div>
+          <div>
+            <h5>Total first tries</h5>
+            <p>{totalFirstTries}</p>
+          </div>
+          <div>
+            <h5>First tries rate</h5>
+            <p>
+              {totalGames !== 0
+                ? ((totalFirstTries / totalGames) * 100).toFixed(2)
+                : 0}{" "}
+              %
+            </p>
+          </div>
+        </div>
+        <h4>Games won per day</h4>
         <div className="pb-4 pt-2">
           <LineChart width={1000} height={500} data={gamesPerDay}>
             <CartesianGrid strokeDasharray="2 2" />
@@ -89,7 +134,7 @@ export default function StatsData() {
             <Line type="monotone" dataKey="Value" stroke="#005A82" />
           </LineChart>
         </div>
-        <h4>Scores per day</h4>
+        <h4>Score development per day</h4>
         <div className="pb-4 pt-2">
           <LineChart width={1000} height={500} data={scoresPerDay}>
             <CartesianGrid strokeDasharray="2 2" />
@@ -99,7 +144,7 @@ export default function StatsData() {
             <Line type="monotone" dataKey="Value" stroke="#005A82" />
           </LineChart>
         </div>
-        <h4>Tries per day</h4>
+        <h4>Guesses per day</h4>
         <div className="pb-4 pt-2">
           <LineChart width={1000} height={500} data={triesPerDay}>
             <CartesianGrid strokeDasharray="2 2" />
