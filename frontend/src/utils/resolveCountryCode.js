723b47f245c90d98694d36e111b3e527e7434d55
Lasse Suomela <suomelasse@gmail.com>
2025-03-09 22:45:18 +0200

    Fix bug in country name resolve

--- frontend/src/utils/resolveCountryCode.js
+++ frontend/src/utils/resolveCountryCode.js
@@ -1,4 +1,7 @@
 export const getCountryName = (countryCode) => {
+  if (typeof countryCode !== "string") {
+    return null;
+  }
   const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
   return regionNames.of(countryCode.toUpperCase()) || countryCode;
 };
