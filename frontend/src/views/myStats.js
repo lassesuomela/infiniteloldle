38d4c7b729922c6d6b5ffb6ade854bdfc8bcd727
Lasse Suomela <suomelasse@gmail.com>
2025-03-23 11:26:20 +0200

    Stats more mobile friendlier

--- frontend/src/views/myStats.js
+++ frontend/src/views/myStats.js
@@ -5,7 +5,7 @@ import { Helmet } from "react-helmet";
 export default function MyStats() {
   return (
     <>
-      <div className="container pb-5">
+      <div className="container pb-5" id="myStats">
         <Helmet>
           <title>Infiniteloldle - My statistics</title>
           <meta
@@ -14,8 +14,14 @@ export default function MyStats() {
           />
         </Helmet>
 
-        <div className="pb-5">
-          <Stats />
+        <h3 className="text-center pb-3 pt-4">My stats</h3>
+
+        <div className="d-grid justify-content-center text-center card p-5">
+          <div className="d-flex justify-content-between pb-4">
+            <div className="pb-5">
+              <Stats />
+            </div>
+          </div>
         </div>
       </div>
     </>
