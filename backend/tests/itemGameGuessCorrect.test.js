b0e63c8d88c2ec1a27e17ed965f6c49b5ab9ef20
Lasse Suomela <suomelasse@gmail.com>
2025-08-03 22:10:20 +0300

    Remove dead imports

--- backend/tests/itemGameGuessCorrect.test.js
+++ backend/tests/itemGameGuessCorrect.test.js
@@ -1,6 +1,5 @@
 const app = require("../app");
 const request = require("supertest");
-const itemModel = require("../models/itemModel");
 const item = require("../models/v2/item");
 const user = require("../models/v2/user");
 
