fd77ebb94e3faf7e2e828d6c77144ebda989adec
Lasse Suomela <suomelasse@gmail.com>
2026-09-18 22:11:16 +0300

    Rename js->ts

--- backend/middleware/requestTracker.ts
+++ backend/middleware/requestTracker.ts
@@ -8,7 +8,7 @@ const DAU_KEY = "stats:dau";
 const TOKENS_KEY = "stats:tokens";
 
 const trackRequests = async (req, res, next) => {
-  if (process.env.NODE_ENV === "test") {
+  if (process.env["NODE_ENV"] === "test") {
     next();
     return;
   }
@@ -23,7 +23,7 @@ const trackRequests = async (req, res, next) => {
 };
 
 const trackDAU = async (req, res, next) => {
-  if (process.env.NODE_ENV === "test") {
+  if (process.env["NODE_ENV"] === "test") {
     next();
     return;
   }
@@ -46,7 +46,7 @@ const trackDAU = async (req, res, next) => {
 
 // Save stats to DB and reset Redis counters
 const saveStats = async () => {
-  if (process.env.NODE_ENV === "test") {
+  if (process.env["NODE_ENV"] === "test") {
     console.log("Skipping stats saving in test environment");
     return;
   }
